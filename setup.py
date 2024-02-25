fd1e3d5cf8c526ad9dd5611a018395715c6532eb
Thinker Lee <Kui-Feng Lee thinker.li@gmail.com>
2024-02-24 20:19:16 -0800

    Use setuptools to build, install, and distribute.

---
+++ setup.py
@@ -0,0 +1,39 @@
+#-----------------------------------------------------------------------------
+# trace_dwarf: setup.py
+#
+# Setup/installation script.
+#
+# K.F. Lee <thinker.li@gmail.com>
+#-----------------------------------------------------------------------------
+import os, sys
+from setuptools import setup
+
+
+try:
+    with open('README.md', 'rt') as readme:
+        description = '\n' + readme.read()
+except IOError:
+    # maybe running setup.py from some other dir
+    description = ''
+
+
+setup(
+    # metadata
+    name='trace_dwarf',
+    description='Python tools for parsing DWARF debug information.',
+    long_description=description,
+    license='BSD',
+    version='0.30',
+    author='K.F. Lee',
+    maintainer='K.F. Lee',
+    author_email='thinker.li@gmail.com',
+    url='https://github.com/ThinkerYzu/trace_dwarf',
+    platforms='Cross Platform',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        ],
+    install_requires=["pyelftools >= 0.30"],
+    scripts=['scripts/mk-dwarf-db.py',
+             'scripts/draw-callflow.py',
+             'scripts/draw-types.py'],
+)
